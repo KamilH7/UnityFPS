3026f4bc541d5205c40a04db530646f67ff07ab0
Kamil Haraf <kamilharaf7@gmail.com>
2023-10-23 15:18:19 +0200

    Added charge meter for charge gun

--- UnityFPS/Assets/Scripts/ShootingSystem/Gun/GunTypes/ChargeGun/ChargeGun.cs
+++ UnityFPS/Assets/Scripts/ShootingSystem/Gun/GunTypes/ChargeGun/ChargeGun.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnityFPS.ShootingSystem
 {
     public class ChargeGun : BaseGun
     {
+        [field: Header("Charge Gun References")]
+        [field: SerializeField]
+        private Image ChargeFillImage { get; set; }
+
         [field: Header("Charge Gun Settings")]
         [field: SerializeField]
         private float ChargePerSecond { get; set; }
@@ -18,6 +23,7 @@ namespace UnityFPS.ShootingSystem
 			base.Initialize();
 
             CurrentLoadedAmmo.Value = 0;
+            ChargeFillImage.fillAmount = 0;
         }
 
 		public override void ShootInputStarted()
@@ -57,6 +63,7 @@ namespace UnityFPS.ShootingSystem
             CurrentCharge = 0;
             CurrentReserveAmmo.Value += CurrentLoadedAmmo.Value;
             CurrentLoadedAmmo.Value = 0;
+            ChargeFillImage.fillAmount = 0;
         }
 
         protected virtual void Update()
@@ -65,6 +72,7 @@ namespace UnityFPS.ShootingSystem
 			{
                 CurrentCharge += ChargePerSecond * Time.deltaTime;
                 CurrentCharge = Mathf.Clamp(CurrentCharge, 0, MaxCharge);
+                ChargeFillImage.fillAmount = CurrentCharge / MagazineSize;
                 AdjustAmmoToChargeState();
             }
 		}
@@ -87,6 +95,7 @@ namespace UnityFPS.ShootingSystem
                 ApplyGunShake(CurrentCharge);
                 CurrentCharge = 0;
                 CurrentLoadedAmmo.Value = 0;
+                ChargeFillImage.fillAmount = 0;
             }
         }
     }
